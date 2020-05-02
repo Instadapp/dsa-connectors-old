d9a2bf5671e5c1a27f194e960ac4961f95e7d815
Thrilok Kumar <thrilok2000@gmail.com>
2020-05-02 20:03:39 +0530

    updated math.sol

--- contracts/common/math.sol
+++ contracts/common/math.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.6.0;
 
 contract DSMath {
+    uint constant WAD = 10 ** 18;
+    uint constant RAY = 10 ** 27;
 
     function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "math-not-safe");
@@ -10,7 +12,6 @@ contract DSMath {
         require(y == 0 || (z = x * y) / y == x, "math-not-safe");
     }
 
-    uint constant WAD = 10 ** 18;
 
     function wmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), WAD / 2) / WAD;
@@ -18,6 +19,14 @@ contract DSMath {
 
     function wdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, WAD), y / 2) / y;
+    }
+
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
     }
 
 }
