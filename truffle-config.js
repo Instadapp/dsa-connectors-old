6bafbdfece150eb3fdf3032bc51381244f78f5d9
Thrilok Kumar <thrilok2000@gmail.com>
2020-05-27 03:35:19 +0530

    solidity version change

--- truffle-config.js
+++ truffle-config.js
@@ -108,7 +108,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "v0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "v0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
