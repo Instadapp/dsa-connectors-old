eb7cb3d7d4289215118efb1afc6fca8fefc8c8ac
Thrilok Kumar <thrilok2000@gmail.com>
2020-06-19 17:18:57 +0530

    Fixed decimal error in curve connector

--- contracts/connectors/curve.sol
+++ contracts/connectors/curve.sol
@@ -113,9 +113,12 @@ contract CurveProtocol is CurveHelpers {
 
         uint _slippageAmt = convert18ToDec(_buyToken.decimals(), wmul(unitAmt, convertTo18(_sellToken.decimals(), _sellAmt)));
 
-        uint _buyAmt = curve.get_dy(getTokenI(sellAddr), getTokenI(buyAddr), _sellAmt);
-
+        // uint _buyAmt = curve.get_dy(getTokenI(sellAddr), getTokenI(buyAddr), _sellAmt);
+        uint initalBal = _buyToken.balanceOf(address(this));
         curve.exchange(getTokenI(sellAddr), getTokenI(buyAddr), _sellAmt, _slippageAmt);
+        uint finalBal = _buyToken.balanceOf(address(this));
+
+        uint _buyAmt = sub(finalBal, initalBal);
 
         setUint(setId, _buyAmt);
 
