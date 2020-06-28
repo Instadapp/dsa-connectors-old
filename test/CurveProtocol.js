bce54aba135442833c8121125907e8ed0e56d716
Lecky Lao <leckylao@gmail.com>
2020-06-28 23:15:51 +1000

    fix formating on connectors/curve.sol; adding @openzeppelin/test-helpers, ganache-cli; fixing test/CurveProtocol.js to pass; added truffle network for tenderly proxy; added .nvmrc to set to node v12 as ganache-cli requires;

--- test/CurveProtocol.js
+++ test/CurveProtocol.js
@@ -2,7 +2,7 @@ const CurveProtocol = artifacts.require('CurveProtocol')
 const daiABI = require('./abi/dai');
 const erc20 = require('./abi/erc20')
 const swap_abi = require('./abi/swap')
-const { ether, balance } = require('openzeppelin-test-helpers');
+const { ether, balance } = require('@openzeppelin/test-helpers');
 
 const BN = require('bn.js')
 
@@ -24,99 +24,106 @@ const swapContract = new web3.eth.Contract(swap_abi, swap)
 const swapToken = '0xC25a3A3b969415c80451098fa907EC722572917F'
 const tokenContract = new web3.eth.Contract(erc20, swapToken)
 
-
-
 contract('Curve Protocol', async accounts => {
 
+  it('should send ether to the user address', async () => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
+    const ethBalanceBefore = await balance.current(userAddress);
+    // Send 0.1 eth to userAddress to have gas to send an ERC20 tx.
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: userAddress,
+      value: ether('0.1')
+    });
+    const ethBalanceAfter = await balance.current(userAddress);
+    expect(+ethBalanceAfter - +ethBalanceBefore).to.equal(+ether('0.1'))
+  });
 
-	it('should send ether to the DAI address', async () => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
-		// Send 0.1 eth to userAddress to have gas to send an ERC20 tx.
-		await web3.eth.sendTransaction({
-		  from: accounts[0],
-		  to: userAddress,
-		  value: ether('0.1')
-		});
-		const ethBalance = await balance.current(userAddress);
-		expect(+ethBalance).to.be.at.least(+ether('0.1'))
-	});
+  it('should transfer DAI to CurveProtocol', async () => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
+    // Get 100 DAI for first 5 accounts
+    // daiAddress is passed to ganache-cli with flag `--unlock`
+    // so we can use the `transfer` method
+    //
+    // Note: This only works as userAddress has 2546221640728945323079640 Dai,
+    // should remove this dependence in the future
+    const daiBalanceUser = await daiContract.methods.balanceOf(userAddress).call();
+    await daiContract.methods
+      .transfer(contract.address, ether('100').toString())
+      .send({ from: userAddress, gasLimit: 800000 });
+    const daiBalance = await daiContract.methods.balanceOf(contract.address).call();
+    expect(+daiBalance).to.be.at.least(+ether('100'))
+  });
 
-	it('should transfer DAI to CurveProtocol', async () => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
-		// Get 100 DAI for first 5 accounts
-		// daiAddress is passed to ganache-cli with flag `--unlock`
-		// so we can use the `transfer` method
-		await daiContract.methods
-			.transfer(contract.address, ether('100').toString())
-			.send({ from: userAddress, gasLimit: 800000 });
-		const daiBalance = await daiContract.methods.balanceOf(contract.address).call();
-		expect(+daiBalance).to.be.at.least(+ether('100'))	
-	});
+  it('should approve DAI to CurveProtocol', async() => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
 
-	it('should approve DAI to CurveProtocol', async() => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
+    await daiContract.methods
+          .approve(contract.address, ether('100').toString())
+          .send({ from: account, gasLimit: 800000 });
+      const daiAllowance = await daiContract.methods.allowance(account, contract.address).call()
+      expect(+daiAllowance).to.be.at.least(+ether('100'))
+  });
 
-		await daiContract.methods
-	        .approve(contract.address, ether('100').toString())
-	        .send({ from: account, gasLimit: 800000 });
-	    const daiAllowance = await daiContract.methods.allowance(account, contract.address).call()
-	    expect(+daiAllowance).to.be.at.least(+ether('100'))
-	});
+  /* Deprecated as CurveProtocol is not ICurve and exchange has been implemented into sell method
+  it('should exchange', async () => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
 
-	it('should exchange', async () => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
+    // Get 100 DAI for first 5 accounts
+    console.log('before');
+    let get_dy = await contract.get_dy.call(0, 1, ether('1').toString())
+    console.log('after');
+    let min_dy = +get_dy * 0.99
+    let receipt = await contract.exchange(0, 1, ether('1').toString(), 1, { from: account })
+    let buyAmount = receipt.logs[0].args.buyAmount.toString()
+    expect(+buyAmount).to.be.at.least(min_dy);
+  });
+  */
 
-		// Get 100 DAI for first 5 accounts
-		let get_dy = await contract.get_dy.call(0, 1, ether('1').toString())
-		let min_dy = +get_dy * 0.99
-		let receipt = await contract.exchange(0, 1, ether('1').toString(), 1, { from: account })
-		let buyAmount = receipt.logs[0].args.buyAmount.toString()
-		expect(+buyAmount).to.be.at.least(min_dy);	
+  /* Deprecated as CurveProtocol is not ICurve and calc_token_amount has been implemented into withdraw method
+  it('should add liquidity', async () => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
 
-	});
+    let amounts = [ether('1').toString(), 0, 0, 0]
+    let token_amount = await contract.calc_token_amount.call(amounts, true)
 
-	it('should add liquidity', async () => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
+    let receipt = await contract.add_liquidity(amounts, 1, { from: account })
+    let mintAmount = receipt.logs[0].args.mintAmount.toString()
+    expect(+mintAmount).to.be.at.least(+mintAmount)
+  })
 
-		let amounts = [ether('1').toString(), 0, 0, 0]
-		let token_amount = await contract.calc_token_amount.call(amounts, true)
+  it('should remove liquidity imbalance', async () => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
 
-		let receipt = await contract.add_liquidity(amounts, 1, { from: account })
-		let mintAmount = receipt.logs[0].args.mintAmount.toString()
-		expect(+mintAmount).to.be.at.least(+mintAmount)
-	})
+    let tokenBalance = await tokenContract.methods.balanceOf(contract.address).call()
+    let receipt = await contract.remove_liquidity_imbalance(["100000000000", 0, 0, 0], { from: account })
+    let burnAmount = receipt.logs[0].args.burnAmount.toString()
+    let tokenBalanceAfter = await tokenContract.methods.balanceOf(contract.address).call()
 
-	it('should remove liquidity imbalance', async () => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
+    //weird Ganache errors sometimes "cannot decode event"
+    console.log(+tokenBalance, +tokenBalanceAfter, +burnAmount)
+    //expect(BN(tokenBalance)).to.be.a.bignumber.equal(BN(tokenBalanceAfter).add(burnAmount))
 
-		let tokenBalance = await tokenContract.methods.balanceOf(contract.address).call()
-		let receipt = await contract.remove_liquidity_imbalance(["100000000000", 0, 0, 0], { from: account })
-		let burnAmount = receipt.logs[0].args.burnAmount.toString()
-		let tokenBalanceAfter = await tokenContract.methods.balanceOf(contract.address).call()
+  })
 
-		//weird Ganache errors sometimes "cannot decode event"
-		console.log(+tokenBalance, +tokenBalanceAfter, +burnAmount)
-		//expect(BN(tokenBalance)).to.be.a.bignumber.equal(BN(tokenBalanceAfter).add(burnAmount))
+  it('should remove liquidity in one coin', async() => {
+    let account = accounts[0]
+    let contract = await CurveProtocol.deployed()
 
-	})
+    let daiBalance = await daiContract.methods.balanceOf(contract.address).call()
+    let receipt = await contract.remove_liquidity_one_coin("100000000000", 0, 1, { from: account })
+    let withdrawnAmount = receipt.logs[0].args.withdrawnAmount.toString()
+    let daiBalanceAfter = await daiContract.methods.balanceOf(contract.address).call()
 
-	it('should remove liquidity in one coin', async() => {
-		let account = accounts[0]
-		let contract = await CurveProtocol.deployed()
-
-		let daiBalance = await daiContract.methods.balanceOf(contract.address).call()
-		let receipt = await contract.remove_liquidity_one_coin("100000000000", 0, 1, { from: account })
-		let withdrawnAmount = receipt.logs[0].args.withdrawnAmount.toString()
-		let daiBalanceAfter = await daiContract.methods.balanceOf(contract.address).call()
-
-		//weird Ganache errors sometimes "cannot decode event"
-		console.log(+daiBalance, +daiBalanceAfter, +withdrawnAmount)
-		//expect(BN(daiBalance)).to.be.a.bignumber.equal(BN(daiBalanceAfter).sub(withdrawnAmount));
-	})
+    //weird Ganache errors sometimes "cannot decode event"
+    console.log(+daiBalance, +daiBalanceAfter, +withdrawnAmount)
+    //expect(BN(daiBalance)).to.be.a.bignumber.equal(BN(daiBalanceAfter).sub(withdrawnAmount));
+  })
+  */
 });
