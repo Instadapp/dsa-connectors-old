e76db4029428cc227a28e2cb16abf0d50d2e126a
Lecky Lao <leckylao@gmail.com>
2020-07-09 21:33:21 +1000

    clean up truffle-config and enable optimiser; update test to mock token

--- migrations/2_deploy_connector.js
+++ migrations/2_deploy_connector.js
@@ -1,12 +1,10 @@
-const CurveProtocol = artifacts.require("CurveProtocol");
-const ConnectSBTCCurve = artifacts.require("ConnectSBTCCurve");
-
-const connectorsABI = require("../test/abi/connectors.json");
-let connectorsAddr = "0xD6A602C01a023B98Ecfb29Df02FBA380d3B21E0c";
-let connectorInstance = new web3.eth.Contract(connectorsABI, connectorsAddr);
+// const CurveProtocol = artifacts.require("CurveProtocol");
+// const ConnectSBTCCurve = artifacts.require("ConnectSBTCCurve");
+const MockContract = artifacts.require("MockContract");
 
 module.exports = async function(deployer) {
-  deployer.deploy(CurveProtocol);
-  let connectorLength = await connectorInstance.methods.connectorLength().call();
-  deployer.deploy(ConnectSBTCCurve, 1, +connectorLength + 1);
+  // deployer.deploy(CurveProtocol);
+  deployer.deploy(MockContract);
+  // let connectorLength = await connectorInstance.methods.connectorLength().call();
+  // deployer.deploy(ConnectSBTCCurve, 1, +connectorLength + 1);
 };
