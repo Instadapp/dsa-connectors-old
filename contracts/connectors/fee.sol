b53d5bd12c5927ad29fb3e216c36648b605a026a
Thrilok Kumar <thrilok2000@gmail.com>
2020-12-17 19:55:09 +0530

    Updated connector id for Fee connector

--- contracts/connectors/fee.sol
+++ contracts/connectors/fee.sol
@@ -46,10 +46,10 @@ contract Setup is DSMath {
     }
 
      /**
-     * @dev Connector ID and Type. TODO: change.
+     * @dev Connector ID and Type
      */
     function connectorID() public pure returns(uint _type, uint _id) {
-        (_type, _id) = (1, 37);
+        (_type, _id) = (1, 67);
     }
 
 }
@@ -59,6 +59,11 @@ contract FeeResolver is Setup {
 
     /**
      * @dev Calculate fee
+     * @param amount token amount to caculate fee.
+     * @param fee fee percentage. Eg: 1% => 1e17, 100% => 1e18.
+     * @param getId Get token amount at this ID from `InstaMemory` Contract.
+     * @param setId Set total amount at this ID in `InstaMemory` Contract.
+     * @param setIdFee Set only fee amount at this ID in `InstaMemory` Contract.
      */
     function calculateFee(uint amount, uint fee, uint getId, uint setId, uint setIdFee) external payable {
         uint _amt = getUint(getId, amount);
